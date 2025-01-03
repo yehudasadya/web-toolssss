d2e53f3e96bcd4289ab6c2789c4d7185023e300f
yehudasadya <aa121232343@gmail.com>
2025-01-03 03:28:34 +0200

    תיקון המחולל

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,7 @@
-import WhatsAppEncoder from './components/WhatsAppEncoder';
+import WhatsAppEncoder from './WhatsAppEncoder';
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-gray-50">
-      <nav className="bg-green-600 text-white p-4">
-        <div className="container mx-auto px-4">
-          <h1 className="text-2xl font-bold text-right">מחולל קישורי וואטסאפ</h1>
-        </div>
-      </nav>
-
-      <div className="container mx-auto p-8">
-        <WhatsAppEncoder />
-      </div>
-    </main>
+    <WhatsAppEncoder />
   );
 }
